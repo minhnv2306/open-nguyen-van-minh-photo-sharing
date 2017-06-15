923986ec62d728259a85a9e9ab5c6cf7b15489c5
minhnv2306 <minhnv2306@gmail.com>
2017-06-15 14:10:53 +0700

    Initialize migrations
    Edit finallllllll

---
+++ project1/database/migrations/2017_05_28_030422_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->increments('image_id');
+            $table->string('scope');
+            $table->string('path');
+            $table->string('description', 200)->nullable();
+            $table->integer('like');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
